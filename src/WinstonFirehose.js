b1ede9207e6ba84296736220e696a54395536da0
Sam Brown <sbrown2594@gmail.com>
2020-04-17 18:21:49 -0700

    Additional tests and fixed some constant functionality.

--- src/WinstonFirehose.js
+++ src/WinstonFirehose.js
@@ -14,7 +14,7 @@ const WinstonFirehose = class WinstonFirehose extends Transport {
     this.topLevelAttributes = options.topLevelAttributes || ['level', 'message', 'timestamp']; // todo: should level and message always be here?
 
     // Allows for the same attribute to be logged with each message for a particular instance of the logger, i.e. application name or version
-    this.constants = {};
+    this.constants = options.constants || {};
     this.formatter = options.formatter || JSON.stringify;
 
     const streamName = options.streamName;
@@ -37,24 +37,24 @@ const WinstonFirehose = class WinstonFirehose extends Transport {
   }
 
   _formatLogObject(level, message, meta){
+    // gather up everything into one single object. constants overwrite everything.
     let logObject = Object.assign({ timestamp: (new Date()).toISOString() }, meta, {level, message}, this.constants);
 
-    // Ensure the log object has a meta property that is an actual object.
+    // Ensure the log transport was properly passed the metadata as an object.
     // First, check for weird types that might get passed in.
     if (logObject.meta !== undefined && logObject.meta !== null
       && (logObject.meta.length !== undefined || typeof logObject.meta !== 'object')) {
       let metaValue = logObject.meta;
       logObject.meta = {meta: metaValue};
-    } else if (!logObject.meta) { // All "surprise" falsy values should have already been caught
+    } else if (!logObject.meta) { // All "surprise" falsy values should have already been caught, so we can be loose here
       logObject.meta = {};
     }
 
+    // assign top level attributes at top level of log object, everything else goes under the "meta" attribute
     const keys = Object.keys(logObject);
-
-    // assign top level attributes at top level, everything else within "meta" attribute
     keys.forEach(key => {
       if (this.topLevelAttributes.indexOf(key) === -1 && key !== 'meta'){
-        logObject.meta[key] = logObject.key;
+        logObject.meta[key] = logObject[key];
         delete logObject[key];
       }
     });
--- test/WinstonFirehose.test.js
+++ test/WinstonFirehose.test.js
@@ -47,38 +47,153 @@ describe('WinstonFirehose', () =>{
 
 
   describe('logging messages', () => {
+    /**
+     * check log string as prepared by transport and turn back into an object
+     * @param  {WinstonTransport} transport log transport
+     * @return {Object}           object of log data
+     */
+    let checkAndParseLogString = (transport) => {
+      let logString = transport.firehoseWrapper.send.getCall(0).args[0];
+      expect(typeof logString).toEqual('string');
+      return JSON.parse(logString);
+    };
+
+    /**
+     * expects timestamp, meta and level attributes
+     * @param  {object} logData JSON parsed log string
+     * @return {undefined}         none
+     */
+    let expectBasicLogAttributes = (logData) => {
+      expect(logData.timestamp).not.toEqual(undefined);
+
+      expect(logData.meta).not.toEqual(undefined);
+      expect(typeof logData.meta).toEqual('object');
+
+      expect(logData.level).not.toEqual(undefined);
+      expect(typeof logData.level).toEqual('string');
+
+      expect(logData.message).not.toEqual(undefined);
+      expect(typeof logData.message).toEqual('string');
+    };
+
     beforeEach(() => {
       // sinon.stub(lib, 'whatever')
+      // stub the actual firehose send function so that we don't hit
       sinon.stub(FirehoseWrapper.prototype, 'send').usingPromise(Promise).resolves();
     });
 
     afterEach(() => {
       // lib.whatever.restore();
+      // restore after each test to reset counts, calling params
       FirehoseWrapper.prototype.send.restore();
     });
 
-    it('logs in the proper format', () =>{
+    it('logs in the proper format by default', () =>{
+      // set up log transport
       const transport = new WinstonFirehose({
         level: "verbose",
         name: 'VerboseWinstonFirehose',
         streamName: 'winston-firehose-test-stream',
-        retryConfig: {},
         firehoseConfig: {region: 'us-east-1'},
-        topLevelAttributes: ['level', 'method', 'timestamp', 'foo']
       });
-      transport.log('info', 'message', {something: 'attributes', foo: 'bar'});
+      // log a message
+      transport.log('info', 'some message');
+
+      // check how the message is being sent to firehose
+      let logData = checkAndParseLogString(transport);
+      expectBasicLogAttributes(logData);
+    });
+
+    it('properly assigns meta attributes', () =>{
+      let something = 'someValue';
+      let foo = 'bar';
+      let req = {statusCode:400, params:'?foo=bar'};
+      // set up log transport
+      const transport = new WinstonFirehose({
+        level: "verbose",
+        name: 'VerboseWinstonFirehose',
+        streamName: 'winston-firehose-test-stream',
+        firehoseConfig: {region: 'us-east-1'},
+        topLevelAttributes: ['message', 'level', 'method', 'timestamp', 'foo']
+      });
+
+      // log a message
+      transport.log('info', 'some message', {something, foo, req});
+
+      // check top level attributes
+      let logData = checkAndParseLogString(transport);
+      expectBasicLogAttributes(logData);
+
+      expect(logData.foo).not.toEqual(undefined);
+      expect(logData.foo).toEqual(foo);
+
+      // check meta attributes
+      expect(logData.meta.something).not.toEqual(undefined);
+      expect(logData.meta.something).toEqual(something);
+
+      expect(logData.meta.req).not.toEqual(undefined);
+      expect(typeof logData.meta.req).toEqual('object');
+      expect(logData.meta.req.statusCode).toEqual(req.statusCode);
+      expect(logData.meta.req.params).toEqual(req.params);
     });
 
     it('logs in the proper format even if a reserved word is accidentally used', () =>{
+      let metaAttribute = 'metaValue';
+      let foo = 'bar';
+
       const transport = new WinstonFirehose({
         level: "verbose",
         name: 'VerboseWinstonFirehose',
         streamName: 'winston-firehose-test-stream',
         firehoseConfig: {region: 'us-east-1'},
-        topLevelAttributes: ['level', 'method', 'timestamp', 'foo']
+        topLevelAttributes: ['message', 'level', 'method', 'timestamp', 'foo']
       });
-      transport.log('info', 'message', {meta: 'attributes', foo: 'bar'});
+      transport.log('info', 'message', {meta: metaAttribute, foo: foo});
+      let logData = checkAndParseLogString(transport);
+
+      // check that normal attributes are set properly
+      expectBasicLogAttributes(logData);
+      expect(logData.foo).not.toEqual(undefined);
+      expect(logData.foo).toEqual(foo);
+
+      // check that the meta attribute is properly nested
+      expect(logData.meta).not.toEqual(undefined);
+      expect(typeof logData.meta).toEqual('object');
+      expect(logData.meta.meta).toEqual(metaAttribute);
     });
 
+    it('logs with constants', () =>{
+      let appVersion = '1.0.1';
+      let appName = 'myApp';
+      let someMeta = 'foobar';
+      const transport = new WinstonFirehose({
+        level: "verbose",
+        name: 'VerboseWinstonFirehose',
+        streamName: 'winston-firehose-test-stream',
+        firehoseConfig: {region: 'us-east-1'},
+        topLevelAttributes: ['message', 'level', 'method', 'timestamp', 'foo', 'app', 'version'],
+        constants: {
+          'app': appName,
+          'version': appVersion,
+          'someMeta': someMeta
+        }
+      });
+      transport.log('info', 'message', {meta: 'attributes', foo: 'bar'});
+      let logData = checkAndParseLogString(transport);
+      expectBasicLogAttributes(logData);
+
+
+      // constants at top level:
+      expect(logData.app).not.toEqual(undefined);
+      expect(logData.app).toEqual(appName);
+
+      expect(logData.version).not.toEqual(undefined);
+      expect(logData.version).toEqual(appVersion);
+
+      // constants nested in "meta":
+      expect(logData.someMeta).toEqual(undefined);
+      expect(logData.meta.someMeta).not.toEqual(undefined);
+      expect(logData.version).toEqual(appVersion);
+    });
   });
 });
